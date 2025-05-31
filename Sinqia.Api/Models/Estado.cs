8e883ed59fb7e814ce4d2913ce79b4230e800c2b
Fernando L. Porfirio <fernando.lporfirio@gmail.com>
2025-05-31 20:13:19 -0300

    feat: add RegiaoId and Regiao properties to Estado model

--- Sinqia.Api/Models/Estado.cs
+++ Sinqia.Api/Models/Estado.cs
@@ -3,5 +3,7 @@ public class Estado
     public int Id { get; set; }
     public string Nome { get; set; }
     public string Sigla { get; set; }
+    public int RegiaoId { get; set; }
+    public Regiao Regiao { get; set; }
     public ICollection<Cidade> Cidades { get; set; }
 }
