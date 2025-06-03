c7c6dd09ec2e93f13f40445882924be68018d0d8
Fernando L. Porfirio <fernando.lporfirio@gmail.com>
2025-06-03 01:53:09 -0300

    fix: add data annotations for validation in PontoTuristico model

--- Sinqia.Api/Models/PontoTuristico.cs
+++ Sinqia.Api/Models/PontoTuristico.cs
@@ -1,10 +1,36 @@
-public class PontoTuristico
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Sinqia.Api.Models
 {
-    public int Id { get; set; }
-    public string Nome { get; set; }
-    public string Descricao { get; set; }
-    public string Localizacao { get; set; }
-    public int CidadeId { get; set; }
-    public Cidade Cidade { get; set; }
-    public DateTime DataCriacao { get; set; }
+    public class PontoTuristico
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Campo Nome deve ter no máximo 100 caracteres.")]
+        public string Nome { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Campo Descrição deve ter no máximo 100 caracteres.")]
+        public string Descricao { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Campo Localização deve ter no máximo 100 caracteres.")]
+        public string Localizacao { get; set; }
+
+        [Required]
+        public int CidadeId { get; set; }
+
+        [JsonIgnore]
+        public Cidade Cidade { get; set; }
+
+        [Required]
+        public DateTime DataCriacao { get; set; }
+
+        public PontoTuristico()
+        {
+            DataCriacao = DateTime.Now;
+        }
+    }
 }
