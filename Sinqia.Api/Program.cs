2c93e3221d30644295f150c27d672915a5ef90ec
Fernando L. Porfirio <fernando.lporfirio@gmail.com>
2025-06-02 22:54:52 -0300

    feat: add database migration and seeding in Program.cs

--- Sinqia.Api/Program.cs
+++ Sinqia.Api/Program.cs
@@ -11,6 +11,13 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+    SeedService.SeedData(dbContext);
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
