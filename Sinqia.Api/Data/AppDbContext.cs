81199e856fa430b1e859927d9caa30d932625d2c
Fernando L. Porfirio <fernando.lporfirio@gmail.com>
2025-06-02 22:54:23 -0300

    feat: add Regiao DbSet to AppDbContext for database management

--- Sinqia.Api/Data/AppDbContext.cs
+++ Sinqia.Api/Data/AppDbContext.cs
@@ -4,6 +4,7 @@ public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
+    public DbSet<Regiao> Regioes { get; set; }
     public DbSet<Estado> Estados { get; set; }
     public DbSet<Cidade> Cidades { get; set; }
     public DbSet<PontoTuristico> PontosTuristicos { get; set; }
