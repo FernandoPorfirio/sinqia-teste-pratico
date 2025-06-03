d450594e0c712e7f7a303cabec0289865519758a
Fernando L. Porfirio <fernando.lporfirio@gmail.com>
2025-06-03 01:53:14 -0300

    fix: add JsonIgnore attribute to Regiao and Cidades properties in Estado model

--- Sinqia.Api/Models/Estado.cs
+++ Sinqia.Api/Models/Estado.cs
@@ -1,9 +1,18 @@
-public class Estado
+using System.Text.Json.Serialization;
+
+namespace Sinqia.Api.Models
 {
-    public int Id { get; set; }
-    public string Nome { get; set; }
-    public string Sigla { get; set; }
-    public int RegiaoId { get; set; }
-    public Regiao Regiao { get; set; }
-    public ICollection<Cidade> Cidades { get; set; }
+    public class Estado
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sigla { get; set; }
+        public int RegiaoId { get; set; }
+
+        [JsonIgnore]
+        public Regiao Regiao { get; set; }
+
+        [JsonIgnore]
+        public ICollection<Cidade> Cidades { get; set; }
+    }
 }
