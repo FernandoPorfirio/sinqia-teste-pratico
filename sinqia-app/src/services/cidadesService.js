cc8a351aa61ff306c6b32d506e9602b74ceeb0b3
Fernando L. Porfirio <fernando.lporfirio@gmail.com>
2025-06-04 00:04:53 -0300

    feat: add cidadesService for fetching cities by state

---
+++ sinqia-app/src/services/cidadesService.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+const API_URL = `${process.env.NEXT_PUBLIC_API_BASE_URL}/cidade`;
+
+export const listarCidades = (estadoId) =>
+  axios.get(API_URL, { params: { estadoId } });
