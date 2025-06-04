df146244be47b68b72a231dd498de0ef6e5d0cfe
Fernando L. Porfirio <fernando.lporfirio@gmail.com>
2025-06-04 00:10:27 -0300

    fix: update listarEstados function to handle fetching states by region

--- sinqia-app/src/services/estadosService.js
+++ sinqia-app/src/services/estadosService.js
@@ -2,5 +2,9 @@ import axios from "axios";
 
 const API_URL = `${process.env.NEXT_PUBLIC_API_BASE_URL}/estado`;
 
-export const listarEstados = (regiaoId) =>
-  axios.get(API_URL, { params: { regiaoId } });
+export const listarEstados = (regiaoId) => {
+  if (regiaoId) {
+    return axios.get(`${API_URL}/regiao/${regiaoId}`);
+  }
+  return axios.get(API_URL);
+};
