6c69c40719773a380614cb652dd5c92532589544
Fernando L. Porfirio <fernando.lporfirio@gmail.com>
2025-06-03 22:40:55 -0300

    feat: enhance PontosTuristicosPage layout with sidebar and header components

--- sinqia-app/src/app/pontos-turisticos/page.js
+++ sinqia-app/src/app/pontos-turisticos/page.js
@@ -1,9 +1,28 @@
 "use client";
 
+import { useState } from "react";
+import Header from "../../components/base/Header";
+import Footer from "../../components/base/Footer";
+import Sidebar from "../../components/base/Sidebar";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+
 export default function PontosTuristicosPage() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
   return (
-    <div className="p-8">
-      <h1 className="text-2xl font-bold mb-4">Pontos Turísticos</h1>
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <Sidebar open={sidebarOpen} onClose={() => setSidebarOpen(false)} />
+      <div className="flex flex-1">
+        <div className="p-2">
+          <IconButton onClick={() => setSidebarOpen(true)} size="large">
+            <MenuIcon />
+          </IconButton>
+        </div>
+        <div className="flex-1 p-8">teste</div>
+      </div>
+      <Footer />
     </div>
   );
 }
