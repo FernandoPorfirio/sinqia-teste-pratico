44c0b593490b29b3c5ccbbcab056ba96d185dc7a
Fernando L. Porfirio <fernando.lporfirio@gmail.com>
2025-06-04 00:10:07 -0300

    fix: update listarCidades function to correctly fetch cities by estadoId

--- sinqia-app/src/services/cidadesService.js
+++ sinqia-app/src/services/cidadesService.js
@@ -3,4 +3,4 @@ import axios from "axios";
 const API_URL = `${process.env.NEXT_PUBLIC_API_BASE_URL}/cidade`;
 
 export const listarCidades = (estadoId) =>
-  axios.get(API_URL, { params: { estadoId } });
+  axios.get(`${API_URL}/estado/${estadoId}`);
