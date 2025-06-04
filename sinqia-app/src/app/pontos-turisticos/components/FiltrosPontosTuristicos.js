77227a2d0b6cca97538c9f1b010106380d93159a
Fernando L. Porfirio <fernando.lporfirio@gmail.com>
2025-06-04 00:54:02 -0300

    feat: enhance FiltrosPontosTuristicos to manage filters with state and search functionality

--- sinqia-app/src/app/pontos-turisticos/components/FiltrosPontosTuristicos.js
+++ sinqia-app/src/app/pontos-turisticos/components/FiltrosPontosTuristicos.js
@@ -1,23 +1,32 @@
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
-import MenuItem from "@mui/material/MenuItem";
+// import MenuItem from "@mui/material/MenuItem";
 
-export default function FiltrosPontosTuristicos() {
+export default function FiltrosPontosTuristicos({ filtros, setFiltros }) {
   return (
     <Box sx={{ display: "flex", gap: 2, mb: 2 }}>
-      <TextField label="Buscar" variant="outlined" size="small" />
-      <TextField select label="Estado" variant="outlined" size="small" sx={{ minWidth: 120 }}>
-        <MenuItem value="">Todos</MenuItem>
-      </TextField>
-      <TextField select label="Cidade" variant="outlined" size="small" sx={{ minWidth: 120 }}>
-        <MenuItem value="">Todas</MenuItem>
-      </TextField>
-      <TextField select label="Categoria" variant="outlined" size="small" sx={{ minWidth: 120 }}>
-        <MenuItem value="">Todas</MenuItem>
-      </TextField>
-      <Button variant="contained">Filtrar</Button>
-      <Button variant="outlined">Limpar</Button>
+      <TextField
+        label="Buscar"
+        variant="outlined"
+        size="small"
+        value={filtros.search}
+        onChange={e => setFiltros(f => ({ ...f, search: e.target.value, page: 1 }))}
+        sx={{ minWidth: 600 }}
+      />
+      <Button variant="contained" onClick={() => setFiltros(f => ({ ...f, page: 1 }))}>
+        Filtrar
+      </Button>
+      <Button variant="outlined" onClick={() => setFiltros({
+        regiaoId: "",
+        estadoId: "",
+        cidadeId: "",
+        search: "",
+        page: 1,
+        limit: 10,
+      })}>
+        Limpar
+      </Button>
     </Box>
   );
 }
