9df5629717a7eec51b7f138859dec807d206d185
Fernando L. Porfirio <fernando.lporfirio@gmail.com>
2025-06-03 21:52:52 -0300

    feat: create PontosTuristicosPage component with initial layout

---
+++ sinqia-app/src/app/pontos-turisticos/page.js
@@ -0,0 +1,9 @@
+"use client";
+
+export default function PontosTuristicosPage() {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold mb-4">Pontos Turísticos</h1>
+    </div>
+  );
+}
